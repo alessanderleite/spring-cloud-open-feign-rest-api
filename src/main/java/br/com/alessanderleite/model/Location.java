07d83d547c28a0c3a8f18ba4baf3cd4fa8d276ea
alessanderleite <alessander.logistica@gmail.com>
2019-09-05 15:36:05 -0300

    create the model class named Location

---
+++ src/main/java/br/com/alessanderleite/model/Location.java
@@ -0,0 +1,25 @@
+package br.com.alessanderleite.model;
+
+public class Location {
+
+	private String status;
+	private Data data;
+	
+	public Location() {}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Data getData() {
+		return data;
+	}
+
+	public void setData(Data data) {
+		this.data = data;
+	}
+}
